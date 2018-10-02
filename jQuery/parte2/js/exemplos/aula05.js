7c131ab10770b4eeb2ca92a64be28c99bf9f7b83
Josiel Alves <josiel.alves@smn.domain>
2018-10-02 11:23:38 -0300

    jQuery Core

---
+++ jQuery/parte2/js/exemplos/aula05.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("tbody tr:not(.sub)").hide();
+    $(".sub th").prepend("<img src='img/mais.jpg' class='maismenos'/>");
+    $("img").click(function(){
+        if($(this).attr("src") == "img/menos.png"){
+            $(this).attr("src", "img/mais.jpg")
+            .parents().siblings("tr").hide();
+        }else{
+            $(this).attr("src", "img/menos.png")
+            .parents().siblings("tr").show();
+            console.log($(this).attr("src", "img/menos.png")
+            .parents().siblings("tr"));
+        }
+    });
+});
