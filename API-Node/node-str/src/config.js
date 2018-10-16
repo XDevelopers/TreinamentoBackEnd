1abc768f6802dae0098fe20e53ab6bbfa7eb78c0
Josiel Alves <josiel.alves@smn.domain>
2018-10-16 16:00:38 -0300

    Fim API

---
+++ API-Node/node-str/src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo a Node Store!';
+
+module.exports = {
+    connectionString: 'mongodb://josiel:josiel123@ds030607.mlab.com:30607/ndstrjosiel',
+    sendgridKey: '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',
+    containerConnectionString: 'TBD'
+}
